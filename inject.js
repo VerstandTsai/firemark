0af319e6f4f268dd3dd79271767c0be7b784b971
Verstand Tsai <hackertsaigenz@gmail.com>
2026-03-23 22:56:51 +0800

    Added hyphens and blockquote stylings

--- inject.js
+++ inject.js
@@ -3,6 +3,7 @@ script.type = 'module';
 script.text = `
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/es/highlight.min.js';
+document.documentElement.lang = 'en';
 const source = document.body.firstElementChild.innerText;
 document.body.innerHTML = marked.parse(source);
 hljs.highlightAll();
