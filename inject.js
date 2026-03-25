7e65a0870dcb9962ea4b1d64905a73cb3263c700
Verstand Tsai <hackertsaigenz@gmail.com>
2026-03-25 23:20:41 +0800

    Incororated MathJax

--- inject.js
+++ inject.js
@@ -1,12 +1,27 @@
-const script = document.createElement('script');
-script.type = 'module';
-script.text = `
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
-import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/es/highlight.min.js';
+const injectSource = (src, onload) => {
+  const script = document.createElement('script');
+  script.src = src;
+  script.onload = onload;
+  document.body.append(script);
+};
+
+const injectInline = (code) => {
+  const script = document.createElement('script');
+  script.text = code;
+  document.body.append(script);
+};
+
+const jsDelivr = 'https://cdn.jsdelivr.net';
+injectSource(`${jsDelivr}/npm/marked/lib/marked.umd.js`, () => {
+  injectInline(`
+    document.body.innerHTML =
+    marked.parse(document.body.firstElementChild.innerText);
+  `);
+  injectSource(`${jsDelivr}/gh/highlightjs/cdn-release/build/highlight.min.js`,
+    () => injectInline(`hljs.highlightAll();`)
+  );
+  injectInline(`window.MathJax = {tex: {inlineMath: {'[+]': [['$', '$']]}}};`);
+  injectSource(`${jsDelivr}/npm/mathjax@4/tex-chtml.js`);
+});
 document.documentElement.lang = 'en';
-const source = document.body.firstElementChild.innerText;
-document.body.innerHTML = marked.parse(source);
-hljs.highlightAll();
-`;
-document.body.append(script);
 
