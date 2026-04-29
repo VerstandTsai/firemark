c94ffe499ec129bca050aff82c58573fd74a7bff
Verstand Tsai <hackertsaigenz@gmail.com>
2026-04-29 10:23:22 +0800

    Added mermaid support

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ document.body.prepend(destination);
 const jsDelivr = 'https://cdn.jsdelivr.net';
 inject(`${jsDelivr}/npm/marked/lib/marked.umd.js`, () => {
   destination.innerHTML = marked.parse(markdown);
+  for (const x of document.getElementsByClassName('language-mermaid')) {
+    const parent = x.parentElement;
+    parent.innerHTML = x.innerHTML;
+    parent.className = 'mermaid';
+  }
+  inject(`${jsDelivr}/npm/mermaid/dist/mermaid.min.js`,
+    () => mermaid.init()
+  );
   inject(`${jsDelivr}/gh/highlightjs/cdn-release/build/highlight.min.js`,
     () => hljs.highlightAll()
   );
